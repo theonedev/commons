35c801beababd0e7beeb5460edbf0fd4331a0bef
Robin Shen <robin@onedev.io>
2019-10-10 22:34:01 +0800

    Move some methods from core to AntlrUtils

--- commons-codeassist/src/main/java/io/onedev/commons/codeassist/AntlrUtils.java
+++ commons-codeassist/src/main/java/io/onedev/commons/codeassist/AntlrUtils.java
@@ -3,6 +3,8 @@ package io.onedev.commons.codeassist;
 import org.antlr.v4.runtime.Lexer;
 import org.antlr.v4.runtime.Parser;
 
+import io.onedev.commons.utils.WordUtils;
+
 public class AntlrUtils {
 
 	public static String getDefaultGrammarFile(Class<?> lexerClass) {
@@ -23,6 +25,19 @@ public class AntlrUtils {
 		} catch (ClassNotFoundException e) {
 			throw new RuntimeException(e);
 		}
+	}
+
+	public static String getLexerRuleName(String[] lexerRuleNames, int rule) {
+		return WordUtils.uncamel(lexerRuleNames[rule-1]).toLowerCase();
+	}
+	
+	public static int getLexerRule(String[] lexerRuleNames, String lexerRuleName) {
+		for (int i=0; i<lexerRuleNames.length; i++) {
+			String each = lexerRuleNames[i];
+			if (WordUtils.uncamel(each).toLowerCase().equals(lexerRuleName))
+				return i+1;
+		}
+		throw new RuntimeException("Unable to find lexer rule: " + lexerRuleName);
 	}
 
 }
