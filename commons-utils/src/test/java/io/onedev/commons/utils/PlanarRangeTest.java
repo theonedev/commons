b557a7dab0bb0faed606f06a8a2ad3540b5e6cc4
Robin Shen <robin@onedev.io>
2021-03-07 11:40:06 +0800

    Fix unit test error

--- commons-utils/src/test/java/io/onedev/commons/utils/PlanarRangeTest.java
+++ commons-utils/src/test/java/io/onedev/commons/utils/PlanarRangeTest.java
@@ -10,12 +10,12 @@ public class PlanarRangeTest {
 	public void test() {
 		PlanarRange range = new PlanarRange("1.2-3.4-8");
 		assertEquals(0, range.getFromRow());
-		assertEquals(4, range.getToColumn());
+		assertEquals(3, range.getToColumn());
 		assertEquals(8, range.getTabWidth());
 		
 		range = new PlanarRange("1.2-3.4");
 		assertEquals(0, range.getFromRow());
-		assertEquals(4, range.getToColumn());
+		assertEquals(3, range.getToColumn());
 		assertEquals(1, range.getTabWidth());
 	}
 
